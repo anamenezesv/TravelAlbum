e158a14e47091c7a3e630f4e415ec1e8eebbf3c9
Ana Menezes <ana.menezes@siteware.com.br>
2023-10-01 22:49:53 -0300

    Item vision with carousel

--- popup.js
+++ popup.js
@@ -1,24 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     const items = document.querySelectorAll(".item");
-//     const popup = document.getElementById("popup");
-//     const popupFrame = document.getElementById("popupFrame");
-//     const closePopup = document.getElementById("closePopup");
-
-//     items.forEach((item) => {
-//         item.addEventListener("click", () => {
-//             const itemId = item.getAttribute("data-item-id");
-//             const itemSrc = `item.html`; // Supondo que seus arquivos se chamem item1.html, item2.html, etc.
-//             popupFrame.src = itemSrc;
-//             popup.classList.add("show-popup");
-//         });
-//     });
-
-//     closePopup.addEventListener("click", () => {
-//         popup.classList.remove("show-popup");
-//     });
-// });
-
-// Quando um item é clicado
 document.querySelectorAll('.item').forEach(function(item, index) {
     item.addEventListener('click', function() {
         // Exibir a modal
@@ -34,9 +13,8 @@ document.querySelectorAll('.item').forEach(function(item, index) {
     });
 });
 
-// Quando o botão de fechar ou o overlay é clicado
+// Fechar modal quando o botão de fechar ou o overlay é clicado
 document.getElementById('closeModal').addEventListener('click', function() {
-    // Ocultar a modal
     document.getElementById('myModal').style.display = 'none';
 });
 
